742ea5b405ce297bec130807be262773a5843208
github username <github email address>
2019-07-23 22:35:09 -0400

    took off navbar

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from './NavBar.js'
 
 import './App.css';
 
@@ -7,7 +6,7 @@ class App extends React.Component{
   render(){
     return (
       <div>
-      <NavBar />
+        <h1>Hello</h1>
       </div>
     )
   }
