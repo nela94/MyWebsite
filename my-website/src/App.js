537ec9cd54a3e17b0e6ae23e67fd424280ff46c1
github username <github email address>
2019-08-06 19:35:37 -0400

    added home component

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import NavBar from './NavBar'
+import NavBar from './NavBar';
+import Home from './Home'
 import './App.css';
 
 class App extends React.Component{
@@ -7,6 +8,8 @@ class App extends React.Component{
     return (
       <div>
           <NavBar />
+          <Home />
+
       </div>
     )
   }
---
+++ my-website/src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Home = () => {
+  return <div>
+          <img src='./Me.jpg' />
+        </div>
+}
+
+export default Home
