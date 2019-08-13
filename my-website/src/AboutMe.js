11df4ce7685624432b9ddab3d20a4ecd925519c2
github username <github email address>
2019-08-13 15:20:45 -0400

    fixed about me css

--- my-website/src/AboutMe.js
+++ my-website/src/AboutMe.js
@@ -3,8 +3,12 @@ import React from 'react'
 class AboutMe extends React.Component{
   render(){
     return(
-      <div>
-        <img src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/67740394_2920479371300718_1175832256028082176_o.jpg?_nc_cat=102&_nc_oc=AQngnY0E8tZZdp94sFJLaJuiR3kNiH_G8J3dd9wT3wmPTLtE31yB4gwbqUs4ObPO3vY&_nc_ht=scontent-lga3-1.xx&oh=3dd657d2f92ba43cc3033653cfb9e454&oe=5DDEBB25' alt="pic of me" />
+      <div className="aboutme">
+        <img className='aboutMePic' src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/67740394_2920479371300718_1175832256028082176_o.jpg?_nc_cat=102&_nc_oc=AQngnY0E8tZZdp94sFJLaJuiR3kNiH_G8J3dd9wT3wmPTLtE31yB4gwbqUs4ObPO3vY&_nc_ht=scontent-lga3-1.xx&oh=3dd657d2f92ba43cc3033653cfb9e454&oe=5DDEBB25' alt="pic of me" />
+        <div className="infoContainer">
+          <h2>Manuela Sanchez</h2>
+          <p>this whill be info of me lbakjsdh sdjfsdljfh fdjsdkfh dshfks fhjkd hkg sjhkjhk hdhsjkf hsdjhkf dfhjl hksj fhjfks hksdhf ksjdhf ksjhfk sjhfkljsdhf lkdjhfksdjhflsj sdhfjlsdhf ljhdljsdhfskjfhbdsfkjdhsjf </p>
+        </div>
       </div>
     )
   }
